167cdf1c9d048d5be20e388da3d682bdc891478f
samuel0088 <samuelfreitas202020@gmail.com>
2026-09-15 22:20:44 -0300

    teste 6 da remocao da pagina de logo na homepage

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: false,
+      manifestFilename: 'manifest-v4.webmanifest',
       workbox: {
         globPatterns: [
           '**/*.{js,css,html,ico,svg,woff2}',
